80937936ded61224e57c45d20f2b0c6fe9de4c0d
Onur Ustunel <onurustunelwork@gmail.com>
2021-03-25 10:41:46 +0300

    initial commit

---
+++ GuideNews/GuideNews/SplashScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuideNews
+//
+//  Created by MacOS on 25.03.2021.
+//
+
+import UIKit
+
+class SplashScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
